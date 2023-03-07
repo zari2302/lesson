96c1583b45b8afb16bc56c368e7153f043aae67d
users <kalmatovazarina1@gmail.com>
2023-03-07 17:12:58 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+        System.out.print("What's suname:");
+        String surName =scanner.nextLine();
+        System.out.print("What's you're name:");
+        String name = scanner.nextLine();
+        System.out.print("age:");
+        int age = scanner.nextInt();
+        System.out.println("How old are you: Surname is : " + surName);
+        System.out.println("you't nome is:" + name);
+        System.out.println( "age:" + age);
+    }
+
+}
